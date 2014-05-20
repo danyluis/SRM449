390390fe4fb4ac05cd3b8b1df2f0395dd9f61053
Dany Farina <dlfarina@gmail.com>
2014-05-19 23:22:29 -0500

    Added project files.

---
+++ src/topCoder/MountainRoad.java
@@ -0,0 +1,129 @@
+package topCoder;
+import java.lang.Math;
+
+
+/*
+ * This is the solution to the first problem of TopCoder's
+ * SRM 449 DIV 2.
+ */
+public class MountainRoad {
+	
+	// Starting points of hypothenuses
+	int[] start = null;
+
+	// Ending points of hypothenuses
+	int[] end = null;
+	
+	/* Checks whether the triangle at position 'element' is
+	 * contained in the triangle at position 'container'
+	 */
+	boolean contains ( int set, int i )
+	{
+		// Just a normal edge check for set belonging
+		return ( start[set] <= start[i] ) &&
+				( end[set] >= end[i] );
+	}
+	
+	/* Returns true if triangles at positions 'a' and 'b' 
+	 * intersect.  False, otherwise.
+	 */
+	boolean intersect ( int a, int b )
+	{
+		return (
+				(start[a] < start[b] && end[a] < end[b]) ||
+				(start[b] < start[a] && end[b] < end[a])
+				);
+	}
+	
+	/*
+	 * Calculates the base of triangle at position 'a'
+	 */
+	int tBase ( int a )
+	{
+		if ( a == -1 ) return 0;
+		return end[a] - start[a];
+	}
+	
+	/*
+	 * Calculates the mountain distance that needs to be walked
+	 * across over the union of right isosceles triangles
+	 * whose hypothenuses are defined by starting and ending
+	 * points specified in 'start' and 'end' respectively.
+	 */
+	public double findDistance(int[] start, int[] end)
+	{
+		// Set local members using received arguments
+		this.start = start;
+		this.end = end;
+		
+		int n = start.length;
+		
+		boolean[] ignore = new boolean[n];
+		
+		for ( int i=0; i<n; i++ )
+			for ( int j=0; j<n; j++ )
+			{
+				if ( i==j || ignore[j] ) continue;
+				if ( ignore[j] = contains(i, j) ) break;
+			}
+		
+		double total = 0d;
+		for ( int i=0; i<n; i++ )
+		{
+			if ( ignore[i] ) continue;
+			
+			int maxleft = -1;
+			int leftEnd = Integer.MIN_VALUE;
+			int leftBase = 0;
+			
+			int maxright = -1;
+			int rightStart = Integer.MAX_VALUE;
+			int rightBase = 0;
+			
+			for ( int l=0; l<n; l++ )
+			{
+				if ( i==l || ignore[l] || !intersect( i, l ) || (end[l] > end[i]) ) continue;
+
+				int lBase = tBase(l);
+				if ( end[l] == leftEnd && lBase > leftBase )
+				{
+					leftBase = lBase;
+					maxleft = l;
+				}
+				else if ( end[l] > leftEnd )
+				{
+					leftBase = lBase;
+					leftEnd = end[l];
+					maxleft = l;
+				}
+			}
+			
+			for ( int r=0; r<n; r++)
+			{
+				if ( i==r || ignore[r] || !intersect( i, r ) || (start[r] < start[i]) ) continue;
+				
+				int rBase = tBase(r);
+				if ( start[r] == rightStart && rBase > rightBase )
+				{
+					rightBase = rBase;
+					maxright = r;
+				}
+				else if ( start[r] < rightStart )
+				{
+					rightBase = rBase;
+					rightStart = start[r];
+					maxright = r;
+				}
+			}
+		
+			total += 2 * tBase(i);
+		
+			if ( maxleft != -1 )
+				total -= (double)( end[maxleft] - start[i] );
+			
+			if ( maxright != -1 )
+				total -= (double)( end[i] - start[maxright] );
+		}
+		return total / Math.sqrt(2d);
+	}
+}
---
+++ src/topCoder/Runner.java
@@ -0,0 +1,23 @@
+package topCoder;
+
+public class Runner {
+
+	public static void main(String[] args) {
+//		int[] start = {1};
+//		int[] end = {7};
+		
+//		int[] start = {0, 3, 4};
+//		int[] end = {5, 9, 6};
+
+		int[] start = {1,4,5,6,-10};
+		int[] end = {101,102,101,100,99};
+		
+//		int[] start = {-5,-3};
+//		int[] end = {-2,-2};
+		
+		MountainRoad mr = new MountainRoad();
+		double len = mr.findDistance(start, end);
+		System.out.println("Length: " + len);
+	}
+
+}
